44ce624e12f090a1dabc1547548f6144bceb65b8
ChristianDominguezVicente <chris_dovi@hotmail.com>
2025-08-09 17:15:11 +0200

    Menu Update

--- Scripts/Menu/Menu.gd
+++ Scripts/Menu/Menu.gd
@@ -6,19 +6,27 @@ extends Control
 func _ready() -> void:
 	main_buttons.visible = true
 	settings.visible = false
+	$MainButtons/Start.grab_focus()
+
+func _input(event) -> void:
+	if event.is_action_released("ui_cancel"):
+		if settings.visible:
+			main_buttons.visible = true
+			settings.visible = false
+			$MainButtons/Start.grab_focus()
+		else:
+			get_tree().quit()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu/Menu.tscn")
 
-
 func _on_settings_pressed() -> void:
 	main_buttons.visible = false
 	settings.visible = true
-
+	$Settings/SettingsContainer/Music/AudioControl.grab_focus()
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_back_settings_pressed() -> void:
 	_ready()
