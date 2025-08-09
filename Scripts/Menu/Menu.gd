fd92d26ce2f3ec5e5ac45a5a4f6bb8840b1cf71a
ChristianDominguezVicente <chris_dovi@hotmail.com>
2025-08-09 11:53:42 +0200

    Menu

---
+++ Scripts/Menu/Menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var settings: Panel = $Settings
+
+func _ready() -> void:
+	main_buttons.visible = true
+	settings.visible = false
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu/Menu.tscn")
+
+
+func _on_settings_pressed() -> void:
+	main_buttons.visible = false
+	settings.visible = true
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_settings_pressed() -> void:
+	_ready()
