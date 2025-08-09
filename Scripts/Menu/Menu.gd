6295683d79f3141243ca0cc2c00b039ba1b58e82
ChristianDominguezVicente <chris_dovi@hotmail.com>
2025-08-09 20:38:35 +0200

    Menu Selector

--- Scripts/Menu/Menu.gd
+++ Scripts/Menu/Menu.gd
@@ -2,23 +2,28 @@ extends Control
 
 @onready var main_buttons: VBoxContainer = $MainButtons
 @onready var settings: Panel = $Settings
+@onready var selector: Panel = $Selector
 
 func _ready() -> void:
 	main_buttons.visible = true
 	settings.visible = false
+	selector.visible = false
 	$MainButtons/Start.grab_focus()
 
 func _input(event) -> void:
 	if event.is_action_released("ui_cancel"):
-		if settings.visible:
+		if settings.visible or selector.visible:
 			main_buttons.visible = true
 			settings.visible = false
+			selector.visible = false
 			$MainButtons/Start.grab_focus()
 		else:
 			get_tree().quit()
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menu/Menu.tscn")
+	main_buttons.visible = false
+	selector.visible = true
+	$Selector/HBoxContainer/Babak.grab_focus()
 
 func _on_settings_pressed() -> void:
 	main_buttons.visible = false
